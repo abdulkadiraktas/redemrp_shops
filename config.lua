204c6b26cdd70ef48094c47bd5b7d8a0bdc08474
Abdulkadir AKTAŞ <iletisim@abdulkadiraktas.com.tr>
2023-06-30 13:34:26 +0300

    Update config.lua
    add backpack shop on config file

--- config.lua
+++ config.lua
@@ -1,19 +1,23 @@
 Config = {}
-Config.BlipName = 'Shop' 
-Config.BlipSprite = 819673798 
-Config.BlipScale = 0.2 
-Config.EnableDistance = 2.0 
+Config.BlipName = 'Shop'
+Config.BlipSprite = 819673798
+Config.BlipScale = 0.2
+Config.EnableDistance = 2.0
 Config.Zones = {
-
 	Basic = {
 		Items = {},
 		Legal = true,
 		Pos = {
-			{x = -324.28,   y = 801.51, z = 117.88},				
+			{x = -324.28,   y = 801.51, z = 117.88},
 		},
 		Spawn = {
-			{x = -323.18,   y = 802.81, z = 117.94},				
+			{x = -323.18,   y = 802.81, z = 117.94},
 		},
 	},
-
+	backpack = {
+		Items = {},
+		Legal = true,
+		Pos = {vector3(-325.5, 797.37, 116.84)},
+		Spawn ={vector3(-326.05, 797.52, 117.73)}
+	},
 }
