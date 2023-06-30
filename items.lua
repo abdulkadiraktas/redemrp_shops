8683873348be6d6ba24ffa5729e43d50b23383a9
Abdulkadir AKTAŞ <iletisim@abdulkadiraktas.com.tr>
2023-06-30 13:34:57 +0300

    Update items.lua
    add backpack item on item.lua with variations

--- items.lua
+++ items.lua
@@ -1,5 +1,36 @@
 ShopItemsData = {
-{store = "Basic" , item = "water" , price = 0.8 , object = "p_water01x"},
+    {
+        store = "Basic" ,
+        item = "water" ,
+        price = 0.8 ,
+        object = "p_water01x"
+    },
+    {
+        store = "backpack",
+        item ="backpack",
+        variations ={
+            {
+                name = "Bag 1",
+                object = "p_ambpack01x",
+                price = 400,
+                rot = vector3(-30.0, 0.0, 100.0)
+            },
+            {
+                name = "Bag 2",
+                object = "p_ambpack02x",
+                price = 300,
+                rot = vector3(-25.0, 0.0, 100)
+            },
+            {
+                name = "Bag 3",
+                object = "p_ambpack05x",
+                price = 200,
+            },
+            {
+                name = "Bag 4",
+                object = "p_ambpack04x",
+                price = 100,
+            },
+        }
+    }
 }
-
-
